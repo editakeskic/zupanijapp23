4d3b40a9e4806b7afea8104595acc1fc9c8b1f54
Edita Keskic <83307312+editakeskic@users.noreply.github.com>
2021-05-14 13:17:32 +0200

    Kreiranje tablica

--- zupanijapp23.sql
+++ zupanijapp23.sql
@@ -1,3 +1,28 @@
 drop database if exists zupanijapp23;
 create database zupanijapp23;
 use zupanijapp23;
+
+create table zupanija(
+	sifra int(11) not null primary key auto_increment,
+	naziv varchar(50) not null,
+	zupan int not null
+);
+
+create table zupan(
+	sifra int(11) not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null
+);
+
+create table opcina(
+	sifra int(11) not null primary key auto_increment,
+	zupanija int not null,
+	naziv varchar(50) not null
+);
+
+create table mjesto(
+	sifra int(11) not null primary key auto_increment,
+	opcina int not null,
+	naziv varchar(50) not null
+);
+
