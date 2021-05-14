0be7c2de49ab6129fa58adf3be136d5b113df09c
Edita Keskic <83307312+editakeskic@users.noreply.github.com>
2021-05-14 14:01:06 +0200

    Insert into

--- zupanijapp23.sql
+++ zupanijapp23.sql
@@ -31,3 +31,50 @@ alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+show tables;
+
+select * from zupan;
+
+#1-3
+insert into zupan(ime,prezime) values
+('Ivo','Ivic'),
+('Ante','Antic'),
+('Ana','Ancic');
+
+select * from zupanija;
+
+#1-3
+insert into zupanija(zupan,naziv) values
+(2,'Osjecko-baranjska'),
+(1,'Vukovarsko-srijemska'),
+(3,'Istarska');
+
+select * from opcina;
+
+#1-6
+insert into opcina(zupanija,naziv) values
+(1,'Osijek'),
+(2,'Vinkovci'),
+(2,'Vukovar'),
+(3,'Porec'),
+(1,'Dakovo'),
+(3,'Rovinj');
+
+select * from mjesto;
+
+#1-12
+insert into mjesto(opcina,naziv) values
+(1,'Brijest'),
+(1,'Tvrdavica'),
+(2,'Vinkovci'),
+(2,'Mirkovci'),
+(3,'Vukovar'),
+(3,'Grabovo'),
+(4,'Porec'),
+(4,'Veleniki'),
+(5,'Dakovo'),
+(5,'Piskorevci'),
+(6,'Rovinj'),
+(6,'Rovinjsko selo');
+
